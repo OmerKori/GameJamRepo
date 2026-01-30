74fd40d48e265d754ac7bde964f3a4d14daf5f20
OmerKori <omer098.013.net.il@gmail.com>
2026-01-30 15:26:38 +0200

    pee

--- GGJ_Mask/Assets/Scripts/MasksManager.cs
+++ GGJ_Mask/Assets/Scripts/MasksManager.cs
@@ -2,33 +2,78 @@ using UnityEngine;
 
 public class MasksManager : MonoBehaviour
 {
-    [SerializeField] GameObject masksParent;
+    [SerializeField] GameObject[] masks;
     [SerializeField] GameObject masksUIParent;
     [SerializeField] RectTransform maskUIHighlight;
-    int currentMaskIndex = 0;
-    int totalMasks = 0;
+    GameObject currentMask,IndicatedMask = null;
+
     private void Start()
     {
-        totalMasks = masksParent.transform.childCount;
+        currentMask = masks[0];
     }
     private void Update()
     {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-
-        if (scroll >= 0.1f)
-            ChangeMask(1);
-        else if (scroll <= -0.1f)
-            ChangeMask(-1);
+        if (Input.GetKeyDown(KeyCode.U))
+            IndicateMask(0);
+        else if (Input.GetKeyDown(KeyCode.I))
+            IndicateMask(1);
+        else if(Input.GetKeyDown(KeyCode.O))
+            IndicateMask(2);
+        else if(Input.GetKeyDown(KeyCode.P))
+            IndicateMask(3);
+            
+        if(Input.GetKeyDown(KeyCode.Space) && IndicatedMask!=null)
+            SwapMasks();
     }
 
-    void ChangeMask(int i)
+    void IndicateMask(int i)
     {
-        currentMaskIndex += i;
-        if(currentMaskIndex > totalMasks)
-            currentMaskIndex = 0;
-        else if (currentMaskIndex < 0)
-            currentMaskIndex = totalMasks;
-        maskUIHighlight.SetParent(masksUIParent.transform.GetChild(currentMaskIndex), false);
+        if (masks[i] == currentMask) 
+            return;
+        
+        if(masks[i] == IndicatedMask)
+        { 
+            RemoveIndicatedMask();
+            return;
+        }
+
+        if (i > masks.Length)
+        {
+            Debug.LogError("Mask index out of range: " + i);
+            return;
+        }
+
+        if(IndicatedMask != null)
+            IndicatedMask.SetActive(false);
+
+        //enable indicated mask and change transparencies
+        IndicatedMask = masks[i];
+        IndicatedMask.GetComponent<PolygonCollider2D>().enabled = false;
+        IndicatedMask.SetActive(true);
+        IndicatedMask.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.8f);
+        currentMask.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+
+        //move highlight in UI
+        maskUIHighlight.SetParent(masksUIParent.transform.GetChild(i), false);
         maskUIHighlight.anchoredPosition = Vector3.zero;
     }
+
+    void SwapMasks()
+    {
+        currentMask.SetActive(false);
+        currentMask = IndicatedMask;
+        currentMask.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1f);
+        currentMask.SetActive(true);
+        IndicatedMask.GetComponent<PolygonCollider2D>().enabled = true;
+
+        IndicatedMask = null;
+    }
+
+    private void RemoveIndicatedMask()
+    {
+        IndicatedMask.SetActive(false);
+        IndicatedMask = null;
+        currentMask.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1f);
+    }
+    
 }
