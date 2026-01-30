cc3e92af5c5326633e7f7a165647ceb969383498
OmerKori <omer098.013.net.il@gmail.com>
2026-01-30 13:14:00 +0200

    bolbol

--- GGJ_Mask/Assets/Scripts/MasksManager.cs
+++ GGJ_Mask/Assets/Scripts/MasksManager.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class MasksManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject masksParent;
+    [SerializeField] GameObject masksUIParent;
+    [SerializeField] RectTransform maskUIHighlight;
+    int currentMaskIndex = 0;
+    int totalMasks = 0;
+    private void Start()
     {
-        
+        totalMasks = masksParent.transform.childCount;
+    }
+    private void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll >= 0.1f)
+            ChangeMask(1);
+        else if (scroll <= -0.1f)
+            ChangeMask(-1);
     }
 
-    // Update is called once per frame
-    void Update()
+    void ChangeMask(int i)
     {
-        
+        currentMaskIndex += i;
+        if(currentMaskIndex > totalMasks)
+            currentMaskIndex = 0;
+        else if (currentMaskIndex < 0)
+            currentMaskIndex = totalMasks;
+        maskUIHighlight.SetParent(masksUIParent.transform.GetChild(currentMaskIndex), false);
+        maskUIHighlight.anchoredPosition = Vector3.zero;
     }
 }
