8cb585f44b9a5e034a69933c70f3497ed4d385de
yam-chay <yam234567@gmail.com>
2026-01-31 12:34:17 +0200

    preview

--- GGJ_Mask/Assets/Scripts/LevelManager.cs
+++ GGJ_Mask/Assets/Scripts/LevelManager.cs
@@ -2,41 +2,20 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    int currentLevel = 1;
     Vector2 startingPos;
-    GameObject player;
+    [SerializeField] GameObject player;
 
     MasksManager masksManager;
     private void Start()
     {
-        player = FindFirstObjectByType<PlayerMovement>().gameObject;
         startingPos = player.transform.position;
         masksManager = FindObjectOfType<MasksManager>();
-
-    }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ResetLevel();
-        }
     }
     public void ResetLevel()
     {
         player.transform.position = startingPos;
-        if(masksManager.indicatedMaskIndex == 0)
-        {
-            masksManager.SwapMasks();
-        }
-        else  if (masksManager.currentMaskIndex != 0)
-        {
-            masksManager.IndicateMask(0);
-            masksManager.SwapMasks();
-        }
-        else if(masksManager.currentMaskIndex != -1)
-        {
-            masksManager.RemoveIndicatedMask();
-        }
-
+        masksManager.IndicateMask(0);
+        masksManager.SwapMasks();
     }
-
 }
