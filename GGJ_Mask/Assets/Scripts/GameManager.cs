f65cb88f1783301917311f529e0d3a99b0c00c24
IdanSheinkman <107989409+IdanSheinkman@users.noreply.github.com>
2026-01-31 17:34:26 +0200

    Pause Menu Level 1

--- GGJ_Mask/Assets/Scripts/GameManager.cs
+++ GGJ_Mask/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     static int currentLevel = 1;
+    [SerializeField] private GameObject pauseMenu;
+    private bool isPaused = false;
+    private bool EscPressed = false;
+
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
@@ -13,5 +18,51 @@ public class GameManager : MonoBehaviour
     {
         currentLevel++;
         SceneManager.LoadScene("Level "+currentLevel);
+    }
+
+
+    private void Update()
+    {
+        EscPressed = Input.GetKeyDown(KeyCode.Escape);
+        if (EscPressed && !isPaused)
+        {
+            isPaused = true;
+            PauseLevel();
+        }
+        else if (EscPressed && isPaused)
+        {
+            isPaused = false;
+            ResumeLevel();
+        }
+    }
+
+    private void ResumeLevel()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    private void PauseLevel()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level " + currentLevel);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
     }
 }
